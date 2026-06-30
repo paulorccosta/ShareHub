25f3ebbe400b3ee651e6108f6bce3c505c6517aa
Paulo <paulorcavalheiroc@gmail.com>
2026-06-30 11:16:06 -0300

    Redirect root to login/dashboard instead of Laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\SpaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route(auth()->check() ? 'dashboard' : 'login');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
